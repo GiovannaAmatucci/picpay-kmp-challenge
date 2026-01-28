af7e0250ebdaa91f783c1df5833ec601ff10f794
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2026-01-28 15:10:29 -0300

    fix(ios): add @ConstructedBy annotation to Database for KMP support

--- composeApp/src/commonMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/AppDatabase.kt
@@ -1,13 +1,17 @@
 package com.giovanna.amatucci.desafio_android_picpay.data.local.db
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import com.giovanna.amatucci.desafio_android_picpay.data.local.db.dao.ContactUserDao
 import com.giovanna.amatucci.desafio_android_picpay.data.local.db.model.ContactUserEntity
-
+@ConstructedBy(AppDatabaseConstructor::class)
 @Database(
     entities = [ContactUserEntity::class], version = 1, exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun contactUserDao(): ContactUserDao
 }
+
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase>
