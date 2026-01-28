7700c2333cd243efd9dfabcce900871f6ac4bd7c
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2026-01-28 16:38:47 -0300

    fix(common): resolve DI crash by injecting Ktor HttpClient directly

--- composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/IosDatabase.kt
+++ composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/IosDatabase.kt
@@ -3,12 +3,11 @@ package com.giovanna.amatucci.desafio_android_picpay.data.local.db
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import platform.Foundation.NSHomeDirectory
-import com.giovanna.amatucci.desafio_android_picpay.data.local.db.AppDatabase_Impl
+
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
     val dbFilePath = NSHomeDirectory() + "/Documents/picpay-db"
     return Room.databaseBuilder<AppDatabase>(
-        name =  dbFilePath,
-        factory = { AppDatabase_Impl() }
+        name =  dbFilePath
     )
 }
