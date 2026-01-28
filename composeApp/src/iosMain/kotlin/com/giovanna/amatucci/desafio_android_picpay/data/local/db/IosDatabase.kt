8bb4a79da11896e57f8d5c2520edd6a74b4f22e4
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2026-01-28 14:14:01 -0300

    fix(ci): use correct gradle task for iOS framework build

--- composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/IosDatabase.kt
+++ composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/data/local/db/IosDatabase.kt
@@ -3,11 +3,12 @@ package com.giovanna.amatucci.desafio_android_picpay.data.local.db
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import platform.Foundation.NSHomeDirectory
+import com.giovanna.amatucci.desafio_android_picpay.data.local.db.AppDatabase_Impl
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
     val dbFilePath = NSHomeDirectory() + "/Documents/picpay-db"
     return Room.databaseBuilder<AppDatabase>(
-        name = dbFilePath,
-        factory = { AppDatabase::class.instantiateImpl() }
+        name =  dbFilePath,
+        factory = { AppDatabase_Impl() }
     )
 }
