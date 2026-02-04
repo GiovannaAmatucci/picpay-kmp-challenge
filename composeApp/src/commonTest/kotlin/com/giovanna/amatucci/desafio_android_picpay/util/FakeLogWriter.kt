6ea21276d40ba3f2c9bcdb1b8f08f0ca17d190ff
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2026-02-04 16:09:37 -0300

    impl(ios): Ios Simulator in CI File

--- composeApp/src/commonTest/kotlin/com/giovanna/amatucci/desafio_android_picpay/util/FakeLogWriter.kt
+++
@@ -1,7 +0,0 @@
-package com.giovanna.amatucci.desafio_android_picpay.util
-
-class FakeLogWriter : LogWriter {
-    override fun d(tag: String, message: String) {}
-    override fun w(tag: String, message: String, t: Throwable?) {}
-    override fun e(tag: String, message: String, t: Throwable?) {}
-}
--- composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/PicPayApplication.kt
+++ composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/PicPayApplication.kt
@@ -2,7 +2,6 @@ package com.giovanna.amatucci.desafio_android_picpay
 
 import com.giovanna.amatucci.desafio_android_picpay.di.initKoin
 import com.giovanna.amatucci.desafio_android_picpay.di.iosModule
-import com.giovanna.amatucci.desafio_android_picpay.di.sharedModules
 
 fun doInitKoin() {
     initKoin {
