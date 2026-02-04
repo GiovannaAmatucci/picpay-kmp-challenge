63b801af60b00f79864fe5b84520d2c1f27a45dd
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2026-02-04 14:19:07 -0300

    fix(ios): fix build errors and optimize CI workflow

--- composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/PicPayApplication.kt
+++ composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/PicPayApplication.kt
@@ -2,6 +2,7 @@ package com.giovanna.amatucci.desafio_android_picpay
 
 import com.giovanna.amatucci.desafio_android_picpay.di.initKoin
 import com.giovanna.amatucci.desafio_android_picpay.di.iosModule
+import com.giovanna.amatucci.desafio_android_picpay.di.sharedModules
 
 fun doInitKoin() {
     initKoin {
--- composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/util/IosLogWriter.kt
+++ composeApp/src/iosMain/kotlin/com/giovanna/amatucci/desafio_android_picpay/util/IosLogWriter.kt
@@ -4,14 +4,16 @@ import platform.Foundation.NSLog
 
 class IosLogWriter : LogWriter {
     override fun d(tag: String, message: String) {
-        NSLog(tag, message)
+        NSLog("%s: %s", tag, message)
     }
 
     override fun w(tag: String, message: String, t: Throwable?) {
-        NSLog(tag, message, t)
+        val errorMessage = t?.toString() ?: ""
+        NSLog("%s: %s Warning: %s", tag, message, errorMessage)
     }
 
     override fun e(tag: String, message: String, t: Throwable?) {
-        NSLog(tag, message, t)
+        val stackTrace = t?.stackTraceToString() ?: ""
+        NSLog("%s: %s Error: %s", tag, message, stackTrace)
     }
 }
